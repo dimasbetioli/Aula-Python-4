43de10e9064a285cb3132d357b124db48727fd33
Dimas Betioli Ribeiro <69821362+dimasbetioli@users.noreply.github.com>
2023-06-12 11:02:33 -0300

    Create Tutorial.py

---
+++ Tutorial.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jun 12 10:25:16 2023
+
+@author: dimas
+"""
+
+word = 'Python'
+"""
+print(word[0])
+print(word[-2:])
+print(word[:0] + word[0:])
+
+lista = [0, 1, 2, 3]
+lista[0] = 2
+print(lista)
+
+word2 = 'Reneltalkngçlankgan' + word[1:]
+print(word2)
+
+print(len(word2))
+"""
+
+squares = ['D', 4, (0, 0), 16, [25, 24]]
+squares.append((2, 3))
+print(squares)
+print(len(squares))
